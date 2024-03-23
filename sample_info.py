c89b73ae91817b3d786feb1c12f9dd9d84115581
HacKer Jr ~ TG <83758692+afluaflu123@users.noreply.github.com>
2024-03-24 03:05:59 +0530

    😡

--- sample_info.py
+++ sample_info.py
@@ -22,3 +22,4 @@ COLLECTION_NAME = 'channel_files'  # If you are using the same database, then us
 
 #temp dict for storing the db uri which will be used for storing user, chat and file infos
 tempDict = {'indexDB': DATABASE_URI}
+
