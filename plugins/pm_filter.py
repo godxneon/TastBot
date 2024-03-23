3d314f2e1c4dbce2626c3643ebb2e7b537ecb2be
HacKer Jr ~ TG <83758692+afluaflu123@users.noreply.github.com>
2024-03-24 02:51:20 +0530

    😡

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -6,13 +6,13 @@ import random
 import pytz
 import datetime
 import time
+import pyrogram
 import psutil, shutil, sys
 lock = asyncio.Lock()
 
 from pyrogram.errors.exceptions.bad_request_400 import MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty
 from Script import script
 from utils import humanbytes
-import pyrogram
 from database.connections_mdb import active_connection, all_connections, delete_connection, if_active, make_active, \
     make_inactive
 from info import ADMINS, AUTH_CHANNEL, FILE_CHANNEL, REACTIONS, AUTH_USERS, CUSTOM_FILE_CAPTION, NOR_IMG, AUTH_GROUPS, P_TTI_SHOW_OFF, IMDB, \
