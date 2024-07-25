56160f002d538e560db53c80a3d99ea82f914814
HacKer Jr ~ TG <83758692+godxneon@users.noreply.github.com>
2024-07-25 12:28:58 +0530

    Update fsub.py

--- plugins/fsub.py
+++ plugins/fsub.py
@@ -173,5 +173,6 @@ async def send_file(client, query, ident, file_id):
         k = await msg.reply("<b>ㅤㅤㅤ❗️❗️<u>IMPORTANT❗️️❗️</u>\n\nThis File Will Be Deleted From Here Within <u>10 Minute</u>. Please Forward This File To Your Saved Messages And Start Download There.</b>",quote=True)
         await asyncio.sleep(60)
         await msg.delete()
-        await k.delete()        
+        await k.delete()    
+        return
    
