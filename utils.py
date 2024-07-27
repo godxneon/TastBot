514bb1a8b713c781d3b19a24a0a033321bfcc0cb
HacKer Jr ~ TG <83758692+godxneon@users.noreply.github.com>
2024-07-27 23:12:23 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -47,7 +47,7 @@ async def check_loop_sub(client, message):
     while True:
         if count == 15:
             return False
-        check = callback_data=f"{mode}#{file_id}")
+        check = await ForceSub(client, message, file_id=file_id, mode=pre)
         count += 1
         if check:
             return True
