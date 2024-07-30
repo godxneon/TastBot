2b2f5f1e1aad4401b7efaaf67003fb1882de06da
HacKer Jr ~ TG <83758692+godxneon@users.noreply.github.com>
2024-07-30 14:19:14 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -53,7 +53,7 @@ async def check_loop_sub(client, message):
             return True
         else:
             pass
-        await asyncio.sleep
+        await asyncio.sleep()
 
 async def is_subscribed(bot, query):
     
