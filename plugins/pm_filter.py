0595753d793b9a09578b25676f2216a590bc3a64
HacKer Jr ~ TG <83758692+godxneon@users.noreply.github.com>
2024-08-05 23:09:44 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -20,7 +20,7 @@ from info import ADMINS, AUTH_CHANNEL, FILE_CHANNEL, REACTIONS, AUTH_USERS, CUST
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery, InputMediaPhoto
 from pyrogram import Client, filters, enums
 from pyrogram.errors import FloodWait, UserIsBlocked, MessageNotModified, PeerIdInvalid
-from utils import get_size, is_subscribed, get_poster, search_gagala, temp, get_settings, save_group_settings
+from utils import get_size, is_subscribed, get_poster, search_gagala, temp, imdb, get_settings, save_group_settings
 from database.users_chats_db import db
 from database.ia_filterdb import Media, get_file_details, get_search_results, get_bad_files
 from database.filters_mdb import (
